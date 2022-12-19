c145cf09d97e5b4140ebe85744015af43b760483
Sudesh <sudeshkolekar2012@gmail.com>
2022-12-19 18:41:16 +0530

    multiple jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-//(5)b5-jsx-multi-tag
-// Multi Tag
+//(6)b6-jsx-multi-jsx
 function App() {
   return (
     <div>
-      <h1>Hellooo</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+// <Person />
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person Name </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
+        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
+        libero! Tempore, consequuntur!
+      </p>
     </div>
   );
 }
