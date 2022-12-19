8885dc429d0096c1f80fbf87ce3e0b9291d1854a
Sudesh <sudeshkolekar2012@gmail.com>
2022-12-19 16:33:21 +0530

    JSX is the hero

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-//(1)b1-project structure
+//(2)b2-jsx
+// JSX!
+// JSX => JAVA SCRIPT + XML
+// XML => Extended the HTML and Cratedad new Tag
+// App => <App />
 function App() {
-  return "Hello Sudesh!";
+  return <h1>Hello Sudesh</h1>;
 }
+
+/*function normalFn() {
+  // ..
+  // ..
+  // ..
+  return 100;
+  return "abcd"
+  return true;
+  return [];
+  return {};
+}*/
 
 export default App;
